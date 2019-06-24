5b1441f390cb0686724371bc6168b14bde228d26
skeyser <skeyser@utexas.edu>
2019-06-24 11:24:43 -0500

    Fixed some of the summary stats.

--- Code/OccModelPlottingVis.R
+++ Code/OccModelPlottingVis.R
@@ -5,27 +5,33 @@ model.sum <- upm1$summary
 
 #rm("upm1")
 
-J <- 274
+J <- 236
 K <- 38
 S <- max(spp.occ$spp.id)
 #Mean detection across species 
-mean.p.sp <- data.frame(matrix(NA, nrow = 5, ncol = 1))
-names(mean.p.sp) <- c("mean.p")
+mean.p.sp <- data.frame(Species = 1:length(unique(spp.occ$Species)), "Mean p No RE" = NA,
+                        "Mean p RE" = NA)
 
+mean.p.sp$Species <- spp.occ$Species
 #Loop throug all species
 for( s in 1:length(unique(spp.occ$spp.id)) ){ 
   #Pull out one species matrix * by non-surveyed sites and years
-  p.sp.mat <- (means.output$p[ s, 1:J, 1:K ]  * JKsurv[ 1:J, 1:K ])
+  p.sp.mat <- (means1$p[ s, 1:J, 1:K ]  * JKmat[ 1:J, 1:K ])
+  p.sp.mat2 <- (means2$p[ s, 1:J, 1:K ]  * JKmat[ 1:J, 1:K ])
   #Sum across sites for each year
-  p.sp.mat <- apply(p.sp.mat, 2, sum)
+  p.sp.mat <- apply(p.sp.mat, 2, sum, na.rm = T)
+  p.sp.mat2 <- apply(p.sp.mat2, 2, sum, na.rm = T)
   #divide by total number of sampled segments each year
   p.sp <-  p.sp.mat[ 1:K ] / surveyedJ[ 1:K ]
+  p.sp2 <-  p.sp.mat2[ 1:K ] / surveyedJ[ 1:K ]
   #Put values into empty DF
-  mean.p.sp [s,] <- mean(p.sp)
+  mean.p.sp [s, 2] <- mean(p.sp)
+  mean.p.sp [s, 3] <- mean(p.sp2)
 }#S
 
 #Pull out just the z matrix
-z.prime <- means.output$z
+z.prime1 <- means1$z
+z.prime2 <-means2$z
 
 #Loop through each species, site, and year to set 0s and 1s 
 #manually using cut-off values
@@ -35,14 +41,16 @@ for (s in 1:S){
     for (k in 1:K){
       #z.prime <- ifelse( mean$z[s, j, k] >= 0.50, 1, 0) 
       #round function rounds to nearest interger so it would do the same as above line
-      z.prime[s, j, k] <- ifelse(means.output$z[s, j, k] > .5, 1, 0) 
+      z.prime1[s, j, k] <- ifelse(means1$z[s, j, k] > .5, 1, 0)
+      z.prime2[s, j, k] <- ifelse(means2$z[s, j, k] > .5, 1, 0) 
     }#K
   }#J
 }#S
 
 #Multiply each species matrix by the NA df for species totals
 for( i in 1:S ){
-  z.prime[ i, , ] <- z.prime[ i, , ] * JKmat
+  z.prime1[ i, , ] <- z.prime1[ i, , ] * JKsurv
+  z.prime2[ i, , ] <- z.prime2[ i, , ] * JKsurv
 }
 
 z.prime[1,,]
@@ -51,11 +59,13 @@ sum(is.na(z.prime[1,,]))
 
 #Find yearly alpha diversity per subgroup
 group.a.div <- matrix(NA, 274, 38)
+group.a.div2 <- matrix(NA, 274, 38)
 
 for (j in 1:J){
   for ( k in  1:K){
     #zeros those z for unsampled segments
-    group.a.div[ j, k ] <- sum( z.prime[ 1:S, j, k ] * JKsurv[ j, k ] )
+    group.a.div[ j, k ] <- sum( z.prime1[ 1:S, j, k ] * JKsurv[ j, k ] )
+    group.a.div2[ j, k ] <- sum( z.prime2[ 1:S, j, k ] * JKsurv[ j, k ] )
     #keep only average estimate
     #mean.a.div[ j, k ] <- mean(a.div[ j, k ])
   }#K
@@ -67,6 +77,15 @@ total <- function(x, y){
   df <- x[y,,]
   yr.sums <- colSums(df, 1:length(nrow))
   sum(yr.sums)
+}
+
+total.observed <- data.frame("Observed" = 1:max(spp.occ$spp.id), "Estimated RE" = NA, 
+                             "Estimated No Sp RE" = NA)
+
+for (i in 1:max(spp.occ$spp.id)){
+  total.observed[i, 1] <- total(ydf, i)
+  total.observed[i, 2] <- total(z.prime1, i)
+  total.observed[i, 3] <- total(z.prime2, i)
 }
 
 #View All non-converged parameters 
