b29526eb73cfbac30591941d3e1d298112086eeb
skeyser <skeyser@utexas.edu>
2019-06-25 17:22:41 -0500

    New file path for final_sp_df.

--- Code/Occupancy_Phylo/Species_Groups_Model_Troubleshooting.R
+++ Code/Occupancy_Phylo/Species_Groups_Model_Troubleshooting.R
@@ -1,7 +1,13 @@
+#################################################################
+###########Script for vsiualizing species groupings##############
+#################################################################
+
+#rm(list = ls())
+
 library("pacman")
 pacman::p_load("tidyverse", "reshape2", "data.table")
 
-final_sp_df <- read.csv(here::here("Data_BBS/Generated DFs/Final_Sp_Df_DetectionsOnly.csv"))
+final_sp_df <- read.csv(here::here("Data_BBS/Generated DFs/Final_Sp_Df_DetectionsOnlyReclass.csv"))
 final_sp_df$Detected <- 1
 
 sp.obs <- aggregate(Detected ~ spp.id, data = final_sp_df, sum)
@@ -14,11 +20,14 @@ hist(sp.obs$Detected, breaks = 8, col = "gray")
 
 final_sp_df <- merge(final_sp_df, sp.obs, by = "spp.id")
 
+
+
 #Trying to figure out the best grouping schemes
 rare <- final_sp_df[final_sp_df$Detected.y < 50,]
 uncommon <- final_sp_df[final_sp_df$Detected.y >= 50 & final_sp_df$Detected.y < 200,]
 common <- final_sp_df[final_sp_df$Detected.y >= 200 & final_sp_df$Detected.y < 1000,]
 abundant <- final_sp_df[final_sp_df$Detected.y >= 1000,] 
+
 
 #Put all different DFs into a list
 sp.groups <- list(rare, uncommon, common, abundant)
@@ -34,6 +43,7 @@ species.levels <- data.frame(Group = c("Rare", "Uncommon", "Common", "Abundant")
 
 #Loop through each DF and pull out each species count
 for (i in 1:4){
+  sp <- 1
   sp[i] <- sp.list(sp.groups, i)
   species.levels[i, 2] <- sp[i]
 }#i
@@ -44,7 +54,11 @@ species.info <- final_sp_df %>% select(c(sci_name, spp.id, BodyMass, Nocturnal,
                                          ForStrat_wataroundsurf,
                                          ForStrat_ground, ForStrat_understory,
                                          ForStrat_midhigh, ForStrat_canopy,
-                                         ForStrat_aerial, Detected.y))
+                                         ForStrat_aerial, Phylo.V1))
+species.info$Detected <- 1
+species.info$sci_name <- as.character(species.info$sci_name)
+species.info <- species.info %>% group_by(sci_name) %>% mutate(Occurences = sum(Detected))
+
 species.info <- species.info[!duplicated(species.info),]
 
 plot(species.info$BodyMass, species.info$Detected.y)
@@ -61,5 +75,18 @@ rt.bcr <- setDT(final_sp_df)[, .(count = uniqueN(rteno.x)), by = BCR]
 yr.bcr <- setDT(final_sp_df)[, .(count = uniqueN(Year)), by = BCR]
 
 
-#save.image(here::here("R Workspace/SpeciesGroupsModTroubleShooting.RData"))
+#Figuring out species 
+group.names <- unique(final_sp_df$Phylo.V1)
+group.names <- as.character(group.names)
+par(mfrow = c(2, 2))
 
+
+for (i in 1:length(unique(group.names))){
+  data.temp <- species.info[species.info$Phylo.V1 == group.names[i],]
+  plots <- barplot(data.temp$Occurences ~ data.temp$spp.id, las =2,
+                   xlab = NA,
+                   main = print(group.names[i]))
+}
+
+
+#save.image(here::here("R Workspace/SpeciesGroupsModTroubleShooting.RData"))
