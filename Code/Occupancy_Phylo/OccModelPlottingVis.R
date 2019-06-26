010c2003e696fcb96e7c99136a272c0f8ab51c0f
skeyser <skeyser@utexas.edu>
2019-06-26 11:42:57 -0500

    Cleaned plotting script and added in some MCMCvis priors to compare to posteriors.

--- Code/Occupancy_Phylo/OccModelPlottingVis.R
+++ Code/Occupancy_Phylo/OccModelPlottingVis.R
@@ -1,13 +1,24 @@
-###Summary Stats for models###
-means.output <- upm1$mean
-Rhats <- upm1$Rhat
-model.sum <- upm1$summary
+###################################################################
+################Summary Stats and plotting#########################
+#############for Multi species occpancy models#####################
+###############Created by : Spencer R Keyser#######################
+###################################################################
 
 #rm("upm1")
+
+#Load in packages
+library(pacman)
+pacman::p_load("MCMCvis", "LaplacesDemon")
+
+#Package loading complete 
 
 J <- 274
 K <- 38
 S <- max(spp.occ$spp.id)
+
+###################################
+#This bit is in the model body now#
+###################################
 
 # #Mean detection across species 
 # mean.p.sp <- data.frame(Species = 1:length(unique(spp.occ$Species)), "Mean p No RE" = NA,
@@ -34,64 +45,12 @@ S <- max(spp.occ$spp.id)
 #   mean.p.sp [s, 3] <- mean(p.sp2)
 # }#S
 
-#Pull out just the z matrix
-z.prime1 <- means1$z
-z.prime2 <-means2$z
+##################################
 
-#Loop through each species, site, and year to set 0s and 1s 
-#manually using cut-off values
-
-for (s in 1:S){
-  for (j in 1:J){
-    for (k in 1:K){
-      #z.prime <- ifelse( mean$z[s, j, k] >= 0.50, 1, 0) 
-      #round function rounds to nearest interger so it would do the same as above line
-      z.prime1[s, j, k] <- ifelse(means1$z[s, j, k] > .5, 1, 0)
-      z.prime2[s, j, k] <- ifelse(means2$z[s, j, k] > .5, 1, 0) 
-    }#K
-  }#J
-}#S
-
-#Multiply each species matrix by the NA df for species totals
-for( i in 1:S ){
-  z.prime1[ i, , ] <- z.prime1[ i, , ] * JKsurv
-  z.prime2[ i, , ] <- z.prime2[ i, , ] * JKsurv
-}
-
-z.prime[1,,]
-z.prime[2,,]
-sum(is.na(z.prime[1,,]))
-
-#Find yearly alpha diversity per subgroup
-group.a.div <- matrix(NA, 274, 38)
-group.a.div2 <- matrix(NA, 274, 38)
-
-for (j in 1:J){
-  for ( k in  1:K){
-    #zeros those z for unsampled segments
-    group.a.div[ j, k ] <- sum( z.prime1[ 1:S, j, k ] * JKsurv[ j, k ] )
-    group.a.div2[ j, k ] <- sum( z.prime2[ 1:S, j, k ] * JKsurv[ j, k ] )
-    #keep only average estimate
-    #mean.a.div[ j, k ] <- mean(a.div[ j, k ])
-  }#K
-}#J
-
-
-#Quick function to calculate total observations of each species
-total <- function(x, y){
-  df <- x[y,,]
-  yr.sums <- colSums(df, 1:length(nrow))
-  sum(yr.sums)
-}
-
-total.observed <- data.frame("Observed" = 1:max(spp.occ$spp.id), "Estimated RE" = NA, 
-                             "Estimated No Sp RE" = NA)
-
-for (i in 1:max(spp.occ$spp.id)){
-  total.observed[i, 1] <- total(ydf, i)
-  total.observed[i, 2] <- total(z.prime1, i)
-  total.observed[i, 3] <- total(z.prime2, i)
-}
+#Extract subsets of the full model for saving the workspace 
+means.output <- upm1$mean
+Rhats <- upm1$Rhat
+model.sum <- upm1$summary
 
 #View All non-converged parameters 
 bad.params <- function(x) {
@@ -100,7 +59,82 @@ bad.params <- function(x) {
   buggers[complete.cases(buggers)]
 }
 
-noconverge <- bad.params(Rhats)
+nonconverge <- bad.params(Rhats)
+
+
+#Pull out just the z matrix
+z.prime <- means1$z
+#z.prime2 <-means2$z
+
+#Loop through each species, site, and year to set 0s and 1s 
+#manually using cut-off values
+
+for (s in 1:S){
+  for (j in 1:J){
+    for (k in 1:K){
+      #Generate multiple "cut-offs" for sensitivity analyses
+      z.prime.5[s, j, k] <- ifelse(means1$z[s, j, k] > 0.5, 1, 0)
+      z.prime.65[s, j, k] <- ifelse(means1$z[s, j, k] > 0.65, 1, 0)
+      z.prime.75[s, j, k] <- ifelse(means1$z[s, j, k] > 0.75, 1, 0)
+      #z.prime2[s, j, k] <- ifelse(means2$z[s, j, k] > .5, 1, 0) 
+    }#K
+  }#J
+}#S
+
+#Multiply each species matrix by the NA df for species totals
+for( i in 1:S ){
+  z.prime.5[ i, , ] <- z.prime.5[ i, , ] * JKsurv
+  z.prime.65[ i, , ] <- z.prime.65[ i, , ] * JKsurv
+  z.prime.75[ i, , ] <- z.prime.75[ i, , ] * JKsurv
+}
+
+#Check differences
+z.prime[1,,]
+z.prime.5[1,,]
+z.prime.65[1,,]
+z.prime.75[1,,]
+
+#Quick function to calculate total observations of each species
+total <- function(x, y){
+  df <- x[y,,]
+  yr.sums <- colSums(df, 1:length(nrow))
+  sum(yr.sums)
+}
+
+#Create empty DF for storing values 
+#total.observed <- data.frame("Observed" = 1:max(spp.occ$spp.id), "Estimated RE" = NA, 
+#                             "Estimated No Sp RE" = NA)
+
+total.observed <- data.frame("Observed" = 1:max(spp.occ$spp.id), "Estimated 0.5" = NA, 
+                             "Estimated 0.65" = NA, "Estimated 0.75" = NA)
+
+for (i in 1:max(spp.occ$spp.id)){
+  total.observed[i, 1] <- total(ydf, i)
+  total.observed[i, 2] <- total(z.prime.5, i)
+  total.observed[i, 3] <- total(z.prime.65, i)
+  total.observed[i, 4] <- total(z.prime.75, i)
+}
+
+#Find yearly alpha diversity per subgroup
+#Generate empty matrices
+group.a.div.5 <- matrix(NA, 274, 38)
+group.a.div.65 <- matrix(NA, 274, 38)
+group.a.div.75 <- matrix(NA, 274, 38)
+
+for (j in 1:J){
+  for ( k in  1:K){
+    #zeros those z for unsampled segments
+    group.a.div.5[ j, k ] <- sum( z.prime.5[ 1:S, j, k ] * JKsurv[ j, k ] )
+    group.a.div.65[ j, k ] <- sum( z.prime.65[ 1:S, j, k ] * JKsurv[ j, k ] )
+    group.a.div.75[ j, k ] <- sum( z.prime.75[ 1:S, j, k ] * JKsurv[ j, k ] )
+    #keep only average estimate
+    #mean.a.div[ j, k ] <- mean(a.div[ j, k ])
+  }#K
+}#J
+
+
+
+
 
 ###########################################################################
 ####################################Plotting###############################
@@ -108,20 +142,62 @@ noconverge <- bad.params(Rhats)
 
 ##Traceplots for relevant parameters
 
-MCMCtrace(upm1, c("alpha", "delta", "int.psi", "int.p", "epsID.psi", "sigma.delta", "sigma.psi", "sigmaID.psi"), Rhat = T, pdf = T, file = here::here("Figues and Tables/TraceSwallows"))
+#Priors used for plotting
+#Alpha params
+PR.norm <- rnorm(1000, 0, 3.126)
+
+#Random effects precision
+sigma.dt <- rhalft(1000, scale = 1, nu = 4)
+prec.df <- 1 / (sigma.dt * sigma.dt)
+
+#Conditional normal for random effects
+PR.norm.cond <- rnorm(1000, 0, prec.dt)
+
+#Random intercepts prior 
+PR.beta <- rbeta(1000, 4, 4)
+
+
+#Plotting 
+MCMCtrace(upm1, params = 'alpha', priors = PR.norm, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'sigma.delta', priors = sigma.dt, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'delta', priors = PR.norm.cond, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'int.p', priors = PR.beta, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'int.psi', priors = PR.beta, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'epsID.psi', priors = PR.norm.cond, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'eps.psi', priors = PR.norm.cond, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'sigmaID.psi', priors = sigma.dt, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
+MCMCtrace(upm1, params = 'sigma.psi', priors = sigma.dt, ind = T, Rhat = T, 
+          n.eff = T, pdf = T, file = here::here("Figures and Tables/TraceSwallows"))
+
 
 
 #Parameter Plots 
 MCMCplot(upm1, params = c("alpha"), rank = T, main = "Swallows Phylo Group Alphas", labels = c("Observer Effect", "Time of Day",
                                                                                                "Day of Year", "Body Mass"))
 
-MCMCplot(upm1, params = c("int.psi", "int.p"), rank = T, main = "Swallows Phylo Group Intercepts", labels = c("Intercept Occupancy", 
+MCMCplot(upm1, params = c("int.psi", "int.p"), rank = T, main = "Phylo Group Intercepts", labels = c("Intercept Occupancy", 
                                                                                                               "Intercept Detection")) 
-MCMCplot(upm1, params = c("epsID.psi", "sigmaID.psi"), main = "Swallows Phylo Group Site Effects")
+MCMCplot(upm1, params = c("epsID.psi", "sigmaID.psi"), main = "Phylo Group Site Effects")
                                                                                                                 
-MCMCplot(upm1, params = c("eps.psi", "sigma.psi"), main = "Swallows Phylo Group Year Effects")
+MCMCplot(upm1, params = c("eps.psi", "sigma.psi"), main = "Phylo Group Year Effects")
 
-MCMCplot(upm1, params = c("delta", "sigma.delta"), main = "Swallows Species RE", labels = c("BASW", "CASW", "CLSW", 
+MCMCplot(upm1, params = c("delta", "sigma.delta"), main = "Species RE", labels = c("BASW", "CASW", "CLSW", 
                                                                                             "PUMA", "BASW", "NRWS", 
                                                                                             "Sigma Delta"))
 
